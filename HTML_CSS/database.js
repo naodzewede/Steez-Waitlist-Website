4d720090e4b3234c0063fb05cb122673a23d7499
Naod Zewede <nzewede@u.rochester.edu>
2024-07-02 09:03:50 -0400

    Changes to be committed (07/02/2024):   (use "git reset HEAD <file>..." to unstage)
        renamed:    HTML_CSS/IPhone-Groups.png -> HTML_CSS/Iphone/IPhone-Groups.png
        renamed:    HTML_CSS/IPhone-Home.png -> HTML_CSS/Iphone/IPhone-Home.png
        renamed:    HTML_CSS/IPhone-Post.png -> HTML_CSS/Iphone/IPhone-Post.png
        new file:   HTML_CSS/database.js
        modified:   HTML_CSS/global.css
        modified:   HTML_CSS/index.css
        modified:   HTML_CSS/index.html
        deleted:    HTML_CSS/newbackground.png
        new file:   HTML_CSS/profilepics/naod.jpg
        new file:   HTML_CSS/profilepics/sam.jpg
        new file:   HTML_CSS/profilepics/troy.png

---
+++ HTML_CSS/database.js
@@ -0,0 +1,51 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDh5quRoFK57eMVP7RAMGE65FTG3re80pI",
+    authDomain: "steez-website.firebaseapp.com",
+    databaseURL: "https://steez-website-default-rtdb.firebaseio.com",
+    projectId: "steez-website",
+    storageBucket: "steez-website.appspot.com",
+    messagingSenderId: "815031721108",
+    appId: "1:815031721108:web:c03d995d801ccf3539e881",
+    measurementId: "G-XEQW7MZ111"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference database
+var waitlistFormDb = firebase.database().ref("waitlistForm");
+
+document.getElementById("waitlistForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    var name = getElementVal('name');
+    var email = getElementVal('email');
+
+    saveForm(name, email);
+
+    document.querySelector(".got-it").style.display = "block";
+
+    document.getElementById('nameText').style.visibility = 'visible';
+
+    document.getElementById('emailText').style.visibility = 'visible';
+
+    setTimeout(() => {
+        document.querySelector(".got-it").style.display = "none";
+    }, 8000);
+
+    document.getElementById("waitlistForm").reset();
+}
+
+const saveForm = (name, email) => {
+    var newWaitlistFormDb = waitlistFormDb.push();
+    newWaitlistFormDb.set({
+        name: name,
+        email: email
+    });
+}
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value;
+}
